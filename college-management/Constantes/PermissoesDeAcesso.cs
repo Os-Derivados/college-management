0a05907613cd01a0f55d063d99368ecbe5737b69
trsaints <thiago.dev2k3@gmail.com>
2024-07-31 17:25:44 -0300

    feat: criar constantes definindo tipos de permissão do sistema

---
+++ college-management/Constantes/PermissoesDeAcesso.cs
@@ -0,0 +1,11 @@
+namespace college_management.Constantes;
+
+public static class PermissoesDeAcesso
+{
+    public const string PermissaoCadastrarAdministradores = "Cadastrar Administradores";
+    public const string PermissaoCadastrarAlunos = "Cadastrar Alunos";
+    public const string PermissaoCadastrarGestores = "Cadastrar Gestores";
+    public const string PermissaoGerenciarMatriculas = "Gerenciar Matriculas";
+    public const string PermissaoGerenciarCursos = "Gerenciar Cursos";
+    public const string PermissaoAcessarCursos = "Acessar Cursos";
+}
