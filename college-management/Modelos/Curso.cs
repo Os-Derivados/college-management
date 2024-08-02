dbe57c8b4d55962cdab042f420d9c76a2523c531
trsaints <thiago.dev2k3@gmail.com>
2024-08-01 22:05:59 -0300

    update: reestruturar lógica de dados

--- college-management/Modelos/Curso.cs
+++ college-management/Modelos/Curso.cs
@@ -5,31 +5,26 @@ namespace college_management.Modelos;
 
 public class Curso
 {
-    private static double _contagemId = 10000000000;
-    public readonly string Id;
-    public string? Nome;
-    private int _cargaHoraria;
-    public Materia[] GradeCurricular;
-
     public Curso(string nome, Materia[] gradeCurricular)
     {
-        Id = _contagemId.ToString(CultureInfo.InvariantCulture); 
         Nome = nome;
+        Id = _contagemId.ToString(CultureInfo.InvariantCulture); 
         GradeCurricular = gradeCurricular;
-
-        foreach (var materia in gradeCurricular)
-        {
-            _cargaHoraria += materia.CargaHoraria;
-        }
-
+        
         _contagemId++;
     }
+    
+    private static double _contagemId = 10000000000;
+    
+    public string? Nome { get; set; }
+    public readonly string Id;
+    public Materia[] GradeCurricular { get; set; }
 
     public override string ToString()
     {
         StringBuilder builder = new();
 
-        builder.AppendLine($"{Nome} - {_cargaHoraria}");
+        builder.AppendLine($"{Nome} - {CalcularCargaHoraria()}");
         builder.AppendLine("Matérias: ");
 
         foreach (var materia in GradeCurricular)
@@ -38,5 +33,10 @@ public class Curso
         }
 
         return builder.ToString();
+    }
+
+    public double CalcularCargaHoraria()
+    {
+        return GradeCurricular.Sum(materia => materia.CargaHoraria);
     }
 }
