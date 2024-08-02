58911acaa5a9119c0d35f8a6d6a81672146edee6
trsaints <thiago.dev2k3@gmail.com>
2024-08-01 22:05:30 -0300

    update: reorganizar estrutura do modelo de dados

--- college-management/Modelos/Materia.cs
+++ college-management/Modelos/Materia.cs
@@ -9,19 +9,21 @@ public enum Turno
 
 public class Materia
 {
-    private static double _contagemId = 10000000000;
-    public readonly string Id;
-    public string? Nome;
-    public readonly int CargaHoraria;
-    public Turno Turno;
-
-    public Materia(string nome, int cargaHoraria, Turno turno)
+    public Materia(string nome, Turno turno, int cargaHoraria)
     {
-        Id = _contagemId.ToString(CultureInfo.InvariantCulture);
         Nome = nome;
-        CargaHoraria = cargaHoraria;
+        Id = _contagemId.ToString(CultureInfo.InvariantCulture);
         Turno = turno;
+        CargaHoraria = cargaHoraria;
 
         _contagemId++;
     }
+    
+    private static double _contagemId = 10000000000;
+    
+    public string? Nome { get; set; }
+    public readonly string? Id;
+    public Turno Turno { get; set; }
+    public int CargaHoraria { get; set; }
+
 }
