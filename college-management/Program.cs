274b51f50815e0c6f2c357bcad6b580eb11dffe7
trsaints <thiago.dev2k3@gmail.com>
2024-08-03 18:20:50 -0300

    remove: apagar fluxo de teste de repositorios

--- college-management/Program.cs
+++ college-management/Program.cs
@@ -1,6 +1,1 @@
-﻿using college_management.Constantes;
-using college_management.Modelos;
-
-var teste = new Usuario("thiago.santos2003", "Thiago Rodrigues", new Cargo(CargosDeAcesso.CargoAlunos), "asdf");
-
-Console.WriteLine(teste.ToString());
+﻿
