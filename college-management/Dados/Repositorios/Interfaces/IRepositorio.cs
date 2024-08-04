cdb5caaa0d93636167c4f3518e16370dc283985a
trsaints <thiago.dev2k3@gmail.com>
2024-08-04 20:49:32 -0300

    update: ajustar métodos de somente leitura para serem síncronos

--- college-management/Dados/Repositorios/Interfaces/IRepositorio.cs
+++ college-management/Dados/Repositorios/Interfaces/IRepositorio.cs
@@ -5,8 +5,8 @@ namespace college_management.Dados.Repositorios.Interfaces;
 public interface IRepositorio<T> : IDisposable where T : Modelo
 {
     Task Adicionar(T modelo);
-    Task<List<T>> ObterTodos();
-    Task<T> ObterPorId(string? id);
+    List<T> ObterTodos();
+    T ObterPorId(string? id);
     Task Atualizar(T modelo);
     Task Remover(string? id);
 }
