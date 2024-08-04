8b884325a0ccb1438671e7662ec613d297cddde5
trsaints <thiago.dev2k3@gmail.com>
2024-08-04 20:50:13 -0300

    refactor: alterar escopo de funcionalidades

--- college-management/Program.cs
+++ college-management/Program.cs
@@ -1,51 +1,17 @@
-﻿using college_management.Constantes;
-using college_management.Dados.Repositorios;
-using college_management.Modelos;
-using college_management.Servicos;
+﻿using college_management.Dados;
+using college_management.Middlewares;
 using college_management.Utilitarios;
 
-var estadoAtual = EstadoDoApp.Contexto;
+BaseDeDados baseDeDados = new();
+Ambiente ambiente = new();
 
-var variaveisDeAmbiente = Ambiente.CarregarVariaveis();
+await Seed.IniciarBaseDeDados(baseDeDados, ambiente.variaveis);
 
-variaveisDeAmbiente.TryGetValue("ADMIN_USER", out var nomeDefault);
-variaveisDeAmbiente.TryGetValue("ADMIN_LOGIN", out var loginDefault);
-variaveisDeAmbiente.TryGetValue("ADMIN_PASSWORD", out var senhaDefault);
+var parametroValido = bool.TryParse(args[0], out var modoDesenvolvimento);
 
-Repositorio<Usuario> baseDeUsuarios = new();
-Repositorio<Cargo> baseDeCargos = new();
+var usuarioLogado = Autenticacao.Login(modoDesenvolvimento, baseDeDados.usuarios);
 
-await baseDeCargos.Adicionar(new Cargo(CargosDeAcesso.CargoAdministradores));
-var cargoDefault = await baseDeCargos.ObterPorId("10000000000");
-
-await baseDeUsuarios.Adicionar(new Usuario(loginDefault, nomeDefault, cargoDefault, senhaDefault));
-var usuarioTeste = await baseDeUsuarios.ObterPorId("10000000000");
-
-/*
-do
-{
-    // Implemente a lógica de realizar login aqui
-} while (estadoAtual is EstadoDoApp.Login);
-*/
-
-do
-{
-    Console.WriteLine("Bem-vindo(a). Selecione um dos contextos abaixo.");
-    ServicoDePermissoes.ListarContextos(usuarioTeste);
-
-    var opcaoEscolhida = Console.ReadLine();
-
-    var opcaoValida = int.TryParse(opcaoEscolhida, out var resultado);
-
-    if (!opcaoValida) continue;
-
-    if (resultado is 0)
-        estadoAtual = EstadoDoApp.Sair;
-    
-    Console.Clear();
-} while (estadoAtual is EstadoDoApp.Contexto);
-
-Console.WriteLine("Saindo...");
+Contexto.Inicializar(usuarioLogado);
 
 public enum EstadoDoApp
 {
