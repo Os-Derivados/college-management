808fe5f1b8d5e17a8b5b0f0933f693e12763bb2f
trsaints <thiago.dev2k3@gmail.com>
2024-08-03 23:08:48 -0300

    refactor: utilizar pseudo-variaveis de ambiente para configuração inicial do app

---
+++ college-management/Utilitarios/Ambiente.cs
@@ -0,0 +1,23 @@
+namespace college_management.Utilitarios;
+
+public static class Ambiente
+{
+    public static Dictionary<string, string> CarregarVariaveis()
+    {
+        Dictionary<string, string> variaveisDeAmbiente = new();
+        var caminhoDoArquivo = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "OsDerivados.CollegeManagement", ".env");
+        
+        foreach (var linha in File.ReadAllLines(caminhoDoArquivo))
+        {
+            var partes = linha.Split('=', StringSplitOptions.RemoveEmptyEntries);
+
+            if (partes.Length is not 2)
+                continue;
+            
+            variaveisDeAmbiente.Add(partes[0], partes[1]);
+        }
+
+        return variaveisDeAmbiente;
+    }
+}
