5db7b456dcb426955d6de86c5df886ff5846a62a
trsaints <thiago.dev2k3@gmail.com>
2024-08-04 21:35:19 -0300

    fix: ajustar acessos de propriedade para serialização

--- college-management/Modelos/Usuario.cs
+++ college-management/Modelos/Usuario.cs
@@ -10,7 +10,7 @@ public sealed class Usuario : Modelo
         Nome = nome;
         Id = _contagemId.ToString(CultureInfo.InvariantCulture);
         Cargo = cargo;
-        _senha = senha;
+        Senha = senha;
 
         _contagemId++;
     }
@@ -21,11 +21,11 @@ public sealed class Usuario : Modelo
     public string? Nome { get; set; }
     public override string? Id { get; set; }
     public Cargo? Cargo { get; set; }
-    public string? _senha { get; set; }
+    public string? Senha { get; set; }
 
     public static bool Autenticar(Usuario usuario, string nomeUsuario, string senha)
     {
-        return (usuario.Login == nomeUsuario) && (usuario._senha == senha);
+        return (usuario.Login == nomeUsuario) && (usuario.Senha == senha);
     }
 
     public override string ToString()
