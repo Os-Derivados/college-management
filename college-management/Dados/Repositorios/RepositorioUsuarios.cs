15135bad51d34027fe62f552f9afe18be28fe7f0
trsaints <thiago.dev2k3@gmail.com>
2024-08-04 21:36:53 -0300

    refactor: remover redundâncias após ajustes na classe base

--- college-management/Dados/Repositorios/RepositorioUsuarios.cs
+++ college-management/Dados/Repositorios/RepositorioUsuarios.cs
@@ -5,11 +5,8 @@ namespace college_management.Dados.Repositorios;
 
 public class RepositorioUsuarios : Repositorio<Usuario>, IRepositorioUsuarios
 {
-    public async Task<Usuario> ObterPorLogin(string login)
+    public Usuario ObterPorLogin(string login)
     {
-        if (_baseDeDados.Count is 0)
-            _baseDeDados = await _servicoDeArquivos.CarregarAssincrono();
-
         return _baseDeDados.FirstOrDefault(u => u.Login == login);
     }
 }
