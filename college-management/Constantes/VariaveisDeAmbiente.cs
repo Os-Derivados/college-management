8c63a1b88b94236a50e513b3ea76960ce6424934
trsaints <thiago.dev2k3@gmail.com>
2024-08-06 07:39:37 -0300

    feat: adicionar constantes para nomear variáveis de ambiente

---
+++ college-management/Constantes/VariaveisDeAmbiente.cs
@@ -0,0 +1,9 @@
+namespace college_management.Constantes;
+
+public static class VariaveisDeAmbiente
+{
+    public const string MasterAdminNome = "ADMIN_USER";
+    public const string MasterAdminLogin = "ADMIN_LOGIN";
+    public const string MasterAdminSenha = "ADMIN_PASSWORD";
+    public const string MasterAdminId = "10000000000";
+}
--- college-management/Utilitarios/Seed.cs
+++ college-management/Utilitarios/Seed.cs
@@ -6,20 +6,34 @@ namespace college_management.Utilitarios;
 
 public static class Seed
 {
-    public static async Task IniciarBaseDeDados(BaseDeDados baseDeDados, Dictionary<string, string> variaveisDeAmbiente)
+    public static async Task IniciarBaseDeDados(
+        BaseDeDados baseDeDados,
+        Dictionary<string, string> variaveisDeAmbiente)
     {
-        variaveisDeAmbiente.TryGetValue("ADMIN_USER", out var nomeDefault);
-        variaveisDeAmbiente.TryGetValue("ADMIN_LOGIN", out var loginDefault);
-        variaveisDeAmbiente.TryGetValue("ADMIN_PASSWORD", out var senhaDefault);
-        
-        var usuarioDefault = baseDeDados.usuarios.ObterPorId("10000000000");
+        variaveisDeAmbiente.TryGetValue(
+            VariaveisDeAmbiente.MasterAdminNome,
+            out var nomeDefault);
+        variaveisDeAmbiente.TryGetValue(
+            VariaveisDeAmbiente.MasterAdminLogin,
+            out var loginDefault);
+        variaveisDeAmbiente.TryGetValue(
+            VariaveisDeAmbiente.MasterAdminSenha,
+            out var senhaDefault);
 
-        if (usuarioDefault is not null) 
+        var usuarioDefault =
+            baseDeDados.usuarios.ObterPorId(VariaveisDeAmbiente
+                .MasterAdminId);
+
+        if (usuarioDefault is not null)
             return;
-        
-        await baseDeDados.cargos.Adicionar(new Cargo(CargosDeAcesso.CargoAdministradores));
-        var cargoDefault = baseDeDados.cargos.ObterPorId("10000000000");
-        
-        await baseDeDados.usuarios.Adicionar(new Usuario(loginDefault, nomeDefault, cargoDefault, senhaDefault));
+
+        await baseDeDados.cargos.Adicionar(
+            new Cargo(CargosDeAcesso.CargoAdministradores));
+        var cargoDefault =
+            baseDeDados.cargos.ObterPorId("10000000000");
+
+        await baseDeDados.usuarios.Adicionar(
+            new Usuario(loginDefault, nomeDefault, cargoDefault,
+                senhaDefault));
     }
 }
