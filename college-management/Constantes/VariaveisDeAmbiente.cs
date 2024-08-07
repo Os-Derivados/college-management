2f0db9652662013106c55aed64421afefd4ba751
trsaints <thiago.dev2k3@gmail.com>
2024-08-07 10:00:20 -0300

    feat: adicionar variáveis de ambiente para carregar usuário de testes no sistema

--- college-management/Constantes/VariaveisDeAmbiente.cs
+++ college-management/Constantes/VariaveisDeAmbiente.cs
@@ -5,5 +5,8 @@ public static class VariaveisDeAmbiente
     public const string MasterAdminNome = "ADMIN_USER";
     public const string MasterAdminLogin = "ADMIN_LOGIN";
     public const string MasterAdminSenha = "ADMIN_PASSWORD";
-    public const string MasterAdminId = "10000000000";
+    
+    public const string UsuarioTesteNome = "TEST_USER";
+    public const string UsuarioTesteLogin = "TEST_LOGIN";
+    public const string UsuarioTesteSenha = "TEST_PASSWORD";
 }
