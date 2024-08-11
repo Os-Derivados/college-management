70a33f575e31d1f4925d9eea7cfea36cbb039991
trsaints <thiago.dev2k3@gmail.com>
2024-08-11 10:54:31 -0300

    raw: adicionar comentários para orientar implementação de contextos

--- college-management/Funcionalidades/Contexto.cs
+++ college-management/Funcionalidades/Contexto.cs
@@ -22,56 +22,134 @@ public sealed class Contexto
             case OperacoesDeContexto.AcessarGradeHoraria:
                 AcessarGradeHoraria();
                 break;
-            
+
             case OperacoesDeContexto.AcessarGradeCurricular:
                 AcessarGradeCurricular();
                 break;
-            
+
             case OperacoesDeContexto.AcessarNotas:
                 AcessarNotas();
                 break;
-            
+
             case OperacoesDeContexto.AcessarFinanceiro:
                 AcessarFinanceiro();
                 break;
-            
+
             case OperacoesDeContexto.AcessarMatricula:
                 AcessarMatricula();
                 break;
-            
+
             case OperacoesDeContexto.AcessarCursos:
                 AcessarCursos();
                 break;
-            
+
             case OperacoesDeContexto.AcessarCargos:
                 AcessarCargos();
                 break;
-            
+
             case OperacoesDeContexto.AcessarMaterias:
                 AcessarMaterias();
                 break;
-            
+
             case OperacoesDeContexto.AcessarUsuarios:
                 AcessarUsuarios();
                 break;
         }
     }
-    
-    private void AcessarGradeHoraria() { throw new NotImplementedException(); }
 
-    private void AcessarGradeCurricular() { throw new NotImplementedException(); }
+    private void AcessarGradeHoraria()
+    {
+        // Se o usuário for um Aluno:
+        // Mostre somente informações dele
+        // Se o usuário for um Funcionário:
+        // Peça para selecionar um curso em específico: ID, Nome
+        throw new NotImplementedException();
+    }
 
-    private void AcessarNotas() { throw new NotImplementedException(); }
+    private void AcessarGradeCurricular()
+    {
+        // Se o usuário for um Aluno:
+        // Mostre somente informações dele
 
-    private void AcessarFinanceiro() { throw new NotImplementedException(); }
+        // Se o usuário for um Funcionário:
+        // Peça para selecionar um curso específico: ID, Nome
+        throw new NotImplementedException();
+    }
 
-    private void AcessarMatricula() { throw new NotImplementedException(); }
+    private void AcessarNotas()
+    {
+        // Se o usuário for um Aluno:
+        // Mostre somente informações dele
 
-    private void AcessarCursos() { throw new NotImplementedException(); }
+        // Se o usuário for um Funcionário:
+        // Peça para selecionar um Aluno em específico:  Matricula, Nome, Login, Id...
 
-    private void AcessarCargos() { throw new NotImplementedException(); }
+        throw new NotImplementedException();
+    }
 
-    private void AcessarMaterias() { throw new NotImplementedException(); }
+    private void AcessarFinanceiro()
+    {
+        // Se o usuário for um Aluno:
+        // Mostre somente informações dele
 
-    private void AcessarUsuarios() { throw new NotImplementedException(); }
+        // Se o usuário for um Funcionário:
+        // Peça para selecionar um Aluno em específico: Matricula, Nome, Login, Id...
+
+        throw new NotImplementedException();
+    }
+
+    private void AcessarMatricula()
+    {
+        // Se o usuário for um Aluno:
+        // Mostre somente informações dele
+
+        // Se o usuário for um Funcionário:
+        // Peça para selecionar um Aluno em específico: Matricula, Nome, Login, Id...
+
+        throw new NotImplementedException();
+    }
+
+    private void AcessarCursos()
+    {
+        // Se o usuário for um Aluno:
+        // Mostre somente informações dele
+
+        // Se o usuário for um Funcionário:
+        // Peça para selecionar um Curso em específico: ID, Nome
+
+        throw new NotImplementedException();
+    }
+
+    private void AcessarCargos()
+    {
+        // Se o usuário for um Aluno:
+        // Recurse a solicitação
+
+        // Se o usuário for um Funcionário:
+        // Peça para selecionar um Cargo em específico: ID, Nome
+
+        throw new NotImplementedException();
+    }
+
+    private void AcessarMaterias()
+    {
+        // Se o usuário for um Aluno:
+        // Mostre somente informações dele: Matérias do curso em que está matriculado
+
+        // Se o usuário for um Funcionário:
+        // Peça para selecionar uma Matéria em específico: ID, Nome
+
+        throw new NotImplementedException();
+    }
+
+    private void AcessarUsuarios()
+    {
+        // Se o usuário for um Aluno:
+        // Mostre somente informações dele
+
+        // Se o usuário for um Funcionário:
+        // Peça para selecionar um Usuário em específico: ID, Nome, Login
+
+        throw new NotImplementedException();
+    }
 }
