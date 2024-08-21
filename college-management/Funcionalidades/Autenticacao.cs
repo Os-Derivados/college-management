90f1193ecd19430279a046e6581fba73a260d15e
trsaints <thiago.dev2k3@gmail.com>
2024-08-21 07:52:42 -0300

    feat: separar funcionalidade de autenticação para implementar interações com a interface

---
+++ college-management/Funcionalidades/Autenticacao.cs
@@ -0,0 +1,43 @@
+using college_management.Constantes;
+using college_management.Dados.Modelos;
+using college_management.Dados.Repositorios;
+
+namespace college_management.Funcionalidades;
+
+public sealed class Autenticacao
+{
+    private readonly RepositorioUsuarios _repositorioUsuarios;
+
+    public Autenticacao(RepositorioUsuarios repositorioUsuarios)
+    {
+        _repositorioUsuarios = repositorioUsuarios;
+    }
+
+    public Usuario Login()
+    {
+        // 1. Pedir o login
+        // 2 Pedir a senha
+        // 3. Verificar se o login existe
+        // 3.1. Se não existir -> Login ou a senha estão inválidos
+        // 3.2. Se o login está válido -> Validar a senha
+        // 3.3. Se a senha estiver errada -> Login ou senha estão inválidos
+        // 3.4. Se a senha estiver válida -> Retornar o usuário encontrado na base de dados
+
+        // TODO:
+        // * Substituir o usuário abaixo por um usuário existente
+        // na base de dados, com base nas informações
+        // recebidas durante a interação
+        return new Aluno("thiago.santos", 
+                         "Thiago Santos", 
+                         new Cargo(CargosDeAcesso.CargoAlunos), 
+                         "senha12345", 
+                         new Matricula(
+                             2412130152, 2, 
+                             new Curso("Ciência da Computação", 
+                                       [new Materia(
+                                           "Cálculo 1", 
+                                           Turno.Noturno, 60
+                                       )]), 
+                             Modalidade.Presencial));
+    }
+}
--- college-management/Middlewares/MiddlewareAutenticacao.cs
+++ college-management/Middlewares/MiddlewareAutenticacao.cs
@@ -1,6 +1,7 @@
 using college_management.Constantes;
 using college_management.Dados.Modelos;
 using college_management.Dados.Repositorios;
+using college_management.Funcionalidades;
 using college_management.Utilitarios;
 
 namespace college_management.Middlewares;
@@ -8,19 +9,20 @@ namespace college_management.Middlewares;
 public static class MiddlewareAutenticacao
 {
     public static Usuario Login(bool modoDesenvolvimento,
-                                RepositorioUsuarios baseDeUsuarios)
+                                RepositorioUsuarios repositorioUsuarios)
     {
-        var estadoAtual = EstadoDoApp.Login;
+        var autenticacao = new Autenticacao(repositorioUsuarios);
+        EstadoDoApp estadoAtual;
         Usuario usuarioLogado;
 
         do
         {
             if (modoDesenvolvimento)
             {
-                _ = Ambiente.Variaveis.TryGetValue(
+                _ = UtilitarioAmbiente.Variaveis.TryGetValue(
                     VariaveisDeAmbiente.UsuarioTesteLogin,
                     out var loginTeste);
-                usuarioLogado = baseDeUsuarios
+                usuarioLogado = repositorioUsuarios
                     .ObterPorLogin(loginTeste);
 
                 if (usuarioLogado is null)
@@ -28,46 +30,14 @@ public static class MiddlewareAutenticacao
                         "Usuário de teste não foi encontrado");
 
                 estadoAtual = EstadoDoApp.Contexto;
-
+                
                 break;
             }
 
-            // 1. Pedir o login dele
-            // 2 Pedir a senha dele
-            // 3. Verificar se o login existe
-            // 3.1. Se não existir -> Login ou a senha estão inválidos
-            // 3.2. Se o login está válido -> Validar a senha
-            // 3.3. Se a senha estiver errada -> Login ou senha estão inválidos
-            // 3.4. Se a senha estiver válida -> Retornar o usuário encontrado na base de dados
-
-            // O valor abaixo é para fins de teste e deve ser substituído por um valor
-            // que seja obtido pelo processo de login:
-
-            usuarioLogado =
-                new Aluno("thiago.santos",
-                          "Thiago Rodrigues",
-                          new Cargo(CargosDeAcesso.CargoAlunos),
-                          "aluno12345",
-                          new Matricula(2412130152,
-                                        2024,
-                                        new Curso(
-                                            "Ciência da Computação",
-                                            [
-                                                new Materia(
-                                                    "Sistemas Digitais",
-                                                    Turno.Noturno, 60)
-                                            ]),
-                                        Modalidade.Presencial));
-
-            // O estado atual só deve mudar se o login for validado.
-            // Caso contrário, o loop deve pular a iteração
-
+            usuarioLogado = autenticacao.Login();
+          
             estadoAtual = EstadoDoApp.Contexto;
         } while (estadoAtual is EstadoDoApp.Login);
-
-        if (usuarioLogado is null)
-            throw new InvalidOperationException(
-                "Credenciais inválidas: não foi possível obter usuário");
 
         return usuarioLogado;
     }
