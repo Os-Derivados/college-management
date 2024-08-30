12a507be2fd60ca5425f9f7b0cdd28897f81c8b7
trsaints <thiago.dev2k3@gmail.com>
2024-08-30 16:03:09 -0300

    refactor: associar ServicoDados a uma inferface

---
+++ college-management/Servicos/Interfaces/IServicoDados.cs
@@ -0,0 +1,8 @@
+namespace college_management.Servicos.Interfaces;
+
+public interface IServicoDados<T>
+{
+    public Task SalvarAssicrono(List<T>? items);
+
+    public Task<List<T>?> CarregarAssincrono();
+}
--- college-management/Servicos/ServicoDados.cs
+++ college-management/Servicos/ServicoDados.cs
@@ -1,17 +1,10 @@
 using System.Text.Json;
-using college_management.Dados.Modelos;
+using college_management.Servicos.Interfaces;
 
 namespace college_management.Servicos;
 
-public sealed class ServicoDados<T> where T : Modelo
+public sealed class ServicoDados<T> : IServicoDados<T>
 {
-    private readonly string _caminhoDoArquivo =
-        Path.Combine(
-            Environment.GetFolderPath(
-                Environment.SpecialFolder.ApplicationData),
-            "OsDerivados.CollegeManagement",
-            $"{typeof(T)}_db.json");
-
     public ServicoDados()
     {
         if (!Directory.Exists(
@@ -22,24 +15,23 @@ public sealed class ServicoDados<T> where T : Modelo
         if (!File.Exists(_caminhoDoArquivo))
             File.Create(_caminhoDoArquivo).Dispose();
     }
+    
+    private readonly string _caminhoDoArquivo =
+        Path.Combine(
+            Environment.GetFolderPath(
+                Environment.SpecialFolder.ApplicationData),
+            "OsDerivados.CollegeManagement",
+            $"{typeof(T).Name}s.json");
 
     public async Task SalvarAssicrono(List<T>? items)
     {
-        await using var fs = File.OpenWrite(_caminhoDoArquivo);
-        await JsonSerializer.SerializeAsync(fs, items);
+        await using var streamArquivo = File.OpenWrite(_caminhoDoArquivo);
+        await JsonSerializer.SerializeAsync(streamArquivo, items);
     }
 
     public async Task<List<T>?> CarregarAssincrono()
     {
-        try
-        {
-            await using var fs = File.OpenRead(_caminhoDoArquivo);
-            return await JsonSerializer.DeserializeAsync<List<T>>(fs);
-        }
-        catch (Exception e) when (e is FileNotFoundException
-                                      or DirectoryNotFoundException)
-        {
-            throw;
-        }
+        await using var streamArquivo = File.OpenRead(_caminhoDoArquivo);
+        return await JsonSerializer.DeserializeAsync<List<T>>(streamArquivo);
     }
 }
