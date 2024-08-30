f0d6054e05f2ed9f2d51c6350a0d330e2f1220d4
trsaints <thiago.dev2k3@gmail.com>
2024-08-30 11:24:02 -0300

    update: comentar requisitos para funcionalidades não implementadas

--- college-management/Contextos/ContextoUsuarios.cs
+++ college-management/Contextos/ContextoUsuarios.cs
@@ -46,12 +46,52 @@ public class ContextoUsuarios : Contexto<Usuario>,
             throw new NotImplementedException();
         }
         
+        // [REQUISITO]: A visualização do Aluno deve ser somente
+        // da Matricula vinculada a ele
+        
         throw new NotImplementedException();
     }
 
     public void VerBoletim(Repositorio<Usuario> repositorio,
                            Usuario usuario)
     {
+        // TODO: Desenvolver um algoritmo para visualizar as Notas de um Aluno
+        // [REQUISITO]: A visualização deve ser no formato relatório
+        // 
+        // Ex.: Ver Boletim do Aluno com Matricula 2401123415
+        //
+        // | MATERIA        | NOTA FINAL | STATUS   |
+        // |----------------|------------|----------|
+        // | Calculo 1      |    9.0     | Aprovado |
+        // | Algebra Linear |    N/A     |   N/A    |
+        
+        if (usuario.Cargo.TemPermissao(
+                PermissoesAcesso.PermissaoAcessoEscrita))
+        {
+            // [REQUISITO]: A visualização do Gestor deve permitir a busca
+            // de uma Aluno em específico na base de dados
+            //
+            // Ex.: Ver Boletim do Aluno com Login == "thiago.santos" 
+            //
+            // Selecione um campo abaixo campo para realizar a busca
+            //
+            // [1] Login
+            // [2] Id
+            // [3] Matricula
+            // 
+            // Sua opção: 1 <- Opção que o usuário escolheu 
+            // ...
+            //
+            // Digite o Login do Aluno: thiago.santos <- Nome
+            // digitado pelo Gestor
+            // ...
+
+            throw new NotImplementedException();
+        }
+        
+        // [REQUISITO]: A visualização do Aluno deve ser somente
+        // da Matricula vinculada a ele
+        
         throw new NotImplementedException();
     }
 
