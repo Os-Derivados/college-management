59d96c3fbec32623a58adcc5e7d10b7a9130cff7
trsaints <thiago.dev2k3@gmail.com>
2024-08-30 11:10:51 -0300

    update: renomer constante de acordo com novo padrão

--- college-management/Constantes/PermissoesAcesso.cs
+++ college-management/Constantes/PermissoesAcesso.cs
@@ -2,8 +2,8 @@ namespace college_management.Constantes;
 
 public static class PermissoesAcesso
 {
-    public const string PermissaoGestaoAdministradores =
-        "Gestão Administradores";
+    public const string PermissaoAcessoAdministradores =
+        "Acesso Administradores";
 
     public const string PermissaoAcessoLeitura = "Acesso Leitura";
     public const string PermissaoAcessoEscrita = "Acesso Escrita";
