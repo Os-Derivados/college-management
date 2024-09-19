420c6a16be07f4e6c98b52d31e0d440ee4c85cac
trsaints <thiago.dev2k3@gmail.com>
2024-09-19 11:21:32 -0300

    feat: criar visualização descritiva (chave/valor) para registros do sistema

---
+++ college-management/Views/DetalhesView.cs
@@ -0,0 +1,21 @@
+using college_management.Dados.Modelos;
+
+namespace college_management.Views;
+
+public class DetalhesView<T> : View where T : Modelo
+{
+    public DetalhesView(string titulo, Dictionary<string, string> detalhes) : base(titulo)
+    {
+        _detalhes = detalhes;
+    }
+    
+    private readonly Dictionary<string, string> _detalhes;
+
+    public override void ConstruirLayout() 
+    {
+        foreach (var detalhe in _detalhes)
+        {
+            Layout.AppendLine($"{detalhe.Key}: {detalhe.Value}");
+        }
+    }
+}
