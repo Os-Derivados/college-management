ff8b5463c76abe35104a219af073e4bf73583246
trsaints <thiago.dev2k3@gmail.com>
2024-09-20 11:42:00 -0300

    update: renomear constantes de acesso

--- college-management/Constantes/AcessosContexto.cs
+++ college-management/Constantes/AcessosContexto.cs
@@ -2,21 +2,21 @@ namespace college_management.Constantes;
 
 public static class AcessosContexto
 {
-    public const string AcessoContextoCursos = "Cursos";
-    public const string AcessoContextoCargos = "Cargos";
-    public const string AcessoContextoMaterias = "Matérias";
-    public const string AcessoContextoUsuarios = "Contas";
+    public const string ContextoCursos = "Cursos";
+    public const string ContextoCargos = "Cargos";
+    public const string ContextoMaterias = "Matérias";
+    public const string ContextoUsuarios = "Contas";
 
     public static readonly string[] AcessoAlunos =
     [
-        AcessoContextoCursos,
-        AcessoContextoMaterias,
-        AcessoContextoUsuarios
+        ContextoCursos,
+        ContextoMaterias,
+        ContextoUsuarios
     ];
 
     public static readonly string[] AcessoGestoresAdministradores =
     [
         ..AcessoAlunos,
-        AcessoContextoCargos
+        ContextoCargos
     ];
 }
