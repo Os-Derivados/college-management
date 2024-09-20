f5783d22a18b25e004f381c2a2ca66d0cb143472
trsaints <thiago.dev2k3@gmail.com>
2024-09-20 13:57:59 -0300

    feat: desenvolver interação para captar dados de cadastro de Usuários

--- college-management/Contextos/ContextoUsuarios.cs
+++ college-management/Contextos/ContextoUsuarios.cs
@@ -1,118 +1,163 @@
+using System.Text;
 using college_management.Constantes;
 using college_management.Contextos.Interfaces;
 using college_management.Dados;
 using college_management.Dados.Modelos;
+using college_management.Views;
+
 
 namespace college_management.Contextos;
+
 
 public class ContextoUsuarios : Contexto<Usuario>,
                                 IContextoUsuarios
 {
-    public ContextoUsuarios(BaseDeDados baseDeDados, Usuario usuarioContexto) : base(baseDeDados, usuarioContexto)
-    {
-    }
+	public ContextoUsuarios(BaseDeDados baseDeDados,
+	                        Usuario     usuarioContexto) :
+		base(baseDeDados,
+		     usuarioContexto) { }
 
-    public void VerMatricula()
-    {
-        // TODO: Desenvolver um algoritmo para visualizar Matricula de um Aluno
-        // [REQUISITO]: A visualização deve ser no formato descritivo
-        // 
-        // Ex.: Ver Matricula 2401123415
-        //
-        // Nome: Thiago
-        // Matricula: 2401123415
-        // Curso: Ciência da Computação
-        // Período: 2
+	public void VerMatricula()
+	{
+		// TODO: Desenvolver um algoritmo para visualizar Matricula de um Aluno
+		// [REQUISITO]: A visualização deve ser no formato descritivo
+		// 
+		// Ex.: Ver Matricula 2401123415
+		//
+		// Nome: Thiago
+		// Matricula: 2401123415
+		// Curso: Ciência da Computação
+		// Período: 2
 
-        if (UsuarioContexto.Cargo.TemPermissao(
-                PermissoesAcesso.PermissaoAcessoEscrita))
-            // [REQUISITO]: A visualização do Gestor deve permitir a busca
-            // de um Aluno em específico na base de dados
-            //
-            // Ex.: Ver Matricula do Aluno com Login == "thiago.santos" 
-            //
-            // [Ver Grade Horária]
-            // Selecione um campo abaixo campo para realizar a busca
-            //
-            // [1] Login
-            // [2] Id
-            // [3] Matricula
-            // 
-            // Sua opção: 1 <- Opção que o usuário escolheu 
-            // ...
-            //
-            // Digite o Login do Aluno: thiago.santos <- Nome
-            // digitado pelo Gestor
-            // ...
-            throw new NotImplementedException();
+		if (UsuarioContexto.Cargo.TemPermissao(PermissoesAcesso
+			                                       .PermissaoAcessoEscrita))
+			// [REQUISITO]: A visualização do Gestor deve permitir a busca
+			// de um Aluno em específico na base de dados
+			//
+			// Ex.: Ver Matricula do Aluno com Login == "thiago.santos" 
+			//
+			// [Ver Grade Horária]
+			// Selecione um campo abaixo campo para realizar a busca
+			//
+			// [1] Login
+			// [2] Id
+			// [3] Matricula
+			// 
+			// Sua opção: 1 <- Opção que o usuário escolheu 
+			// ...
+			//
+			// Digite o Login do Aluno: thiago.santos <- Nome
+			// digitado pelo Gestor
+			// ...
+			throw new NotImplementedException();
 
-        // [REQUISITO]: A visualização do Aluno deve ser somente
-        // da Matricula vinculada a ele
+		// [REQUISITO]: A visualização do Aluno deve ser somente
+		// da Matricula vinculada a ele
 
-        throw new NotImplementedException();
-    }
+		throw new NotImplementedException();
+	}
 
-    public void VerBoletim()
-    {
-        // TODO: Desenvolver um algoritmo para visualizar as Notas de um Aluno
-        // [REQUISITO]: A visualização deve ser no formato relatório
-        // 
-        // Ex.: Ver Boletim do Aluno com Matricula 2401123415
-        //
-        // | MATERIA        | NOTA FINAL | STATUS   |
-        // |----------------|------------|----------|
-        // | Calculo 1      |    9.0     | Aprovado |
-        // | Algebra Linear |    N/A     |   N/A    |
+	public void VerBoletim()
+	{
+		// TODO: Desenvolver um algoritmo para visualizar as Notas de um Aluno
+		// [REQUISITO]: A visualização deve ser no formato relatório
+		// 
+		// Ex.: Ver Boletim do Aluno com Matricula 2401123415
+		//
+		// | MATERIA        | NOTA FINAL | STATUS   |
+		// |----------------|------------|----------|
+		// | Calculo 1      |    9.0     | Aprovado |
+		// | Algebra Linear |    N/A     |   N/A    |
 
-        if (UsuarioContexto.Cargo.TemPermissao(
-                PermissoesAcesso.PermissaoAcessoEscrita))
-            // [REQUISITO]: A visualização do Gestor deve permitir a busca
-            // de uma Aluno em específico na base de dados
-            //
-            // Ex.: Ver Boletim do Aluno com Login == "thiago.santos" 
-            //
-            // Selecione um campo abaixo campo para realizar a busca
-            //
-            // [1] Login
-            // [2] Id
-            // [3] Matricula
-            // 
-            // Sua opção: 1 <- Opção que o usuário escolheu 
-            // ...
-            //
-            // Digite o Login do Aluno: thiago.santos <- Nome
-            // digitado pelo Gestor
-            // ...
-            throw new NotImplementedException();
+		if (UsuarioContexto.Cargo.TemPermissao(PermissoesAcesso
+			                                       .PermissaoAcessoEscrita))
+			// [REQUISITO]: A visualização do Gestor deve permitir a busca
+			// de uma Aluno em específico na base de dados
+			//
+			// Ex.: Ver Boletim do Aluno com Login == "thiago.santos" 
+			//
+			// Selecione um campo abaixo campo para realizar a busca
+			//
+			// [1] Login
+			// [2] Id
+			// [3] Matricula
+			// 
+			// Sua opção: 1 <- Opção que o usuário escolheu 
+			// ...
+			//
+			// Digite o Login do Aluno: thiago.santos <- Nome
+			// digitado pelo Gestor
+			// ...
+			throw new NotImplementedException();
 
-        // [REQUISITO]: A visualização do Aluno deve ser somente
-        // da Matricula vinculada a ele
+		// [REQUISITO]: A visualização do Aluno deve ser somente
+		// da Matricula vinculada a ele
 
-        throw new NotImplementedException();
-    }
+		throw new NotImplementedException();
+	}
 
-    public void VerFinanceiro()
-    {
-        throw new NotImplementedException();
-    }
+	public void VerFinanceiro()
+	{
+		throw new NotImplementedException();
+	}
 
-    public override async Task Cadastrar()
-    {
-        throw new NotImplementedException();
-    }
+	public override async Task Cadastrar()
+	{
+		InputView inputUsuario =
+			new("Cadastrar Usuário", "Insira o Nome: ");
 
-    public override async Task Editar()
-    {
-        throw new NotImplementedException();
-    }
+		inputUsuario.ConstruirLayout();
 
-    public override async Task Excluir()
-    {
-        throw new NotImplementedException();
-    }
+		inputUsuario.LerEntrada("Nome");
+		inputUsuario.LerEntrada("Login", "Insira o Login: ");
+		inputUsuario.LerEntrada("Senha", "Insira a Senha: ");
+		inputUsuario.LerEntrada("Cargo", "Insira o Cargo: ");
 
-    public override void Visualizar()
-    {
-        throw new NotImplementedException();
-    }
+		if (inputUsuario.ObterEntrada("Cargo") is CargosPadrao
+			    .CargoAlunos)
+		{
+			inputUsuario.LerEntrada("Matricula",
+			                        "Insira a Matrícula: ");
+
+			inputUsuario.LerEntrada("Periodo",
+			                        "Insira o Período: ");
+
+			inputUsuario.LerEntrada("Curso",
+			                        "Insira o nome do Curso: ");
+
+			inputUsuario.LerEntrada("Modalidade",
+			                        "Insira a Modalidade: ");
+		}
+
+		DetalhesView detalhesView = new("Confirmar Cadastro",
+		                                inputUsuario.EntradasUsuario);
+		
+		detalhesView.ConstruirLayout();
+
+		StringBuilder mensagemConfirmacao = new();
+		mensagemConfirmacao.AppendLine(detalhesView.Layout.ToString());
+		mensagemConfirmacao.Append("[S] Sim\t[N] Não: ");
+
+		InputView inputConfirmacao
+			= new("Confirmar Cadastro de Usuário",
+			      mensagemConfirmacao.ToString());
+		
+		inputConfirmacao.ConstruirLayout();
+		inputConfirmacao.LerEntrada("Confirma");
+	}
+
+	public override async Task Editar()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override async Task Excluir()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void Visualizar()
+	{
+		throw new NotImplementedException();
+	}
 }
--- college-management/Views/Interfaces/IInputView.cs
+++ college-management/Views/Interfaces/IInputView.cs
@@ -1,6 +1,9 @@
 namespace college_management.Views.Interfaces;
 
+
 public interface IInputView
 {
-    public void LerEntrada(string chave, string? mensagem);
+	public void LerEntrada(string chave, string? mensagem);
+
+	public string ObterEntrada(string chave);
 }
