9208cfda88ae51e08e04d391e828e98febc3c6ba
trsaints <thiago.dev2k3@gmail.com>
2024-09-24 14:05:56 -0300

    feat: incorporar escolha de opções em componente MenuView

--- college-management/Views/MenuView.cs
+++ college-management/Views/MenuView.cs
@@ -1,13 +1,15 @@
 using System.Text;
+using college_management.Views.Interfaces;
 
 
 namespace college_management.Views;
 
 
-public class MenuView : View
+public class MenuView : View, IMenuView
 {
 	private readonly string   _cabecalho;
 	private readonly string[] _opcoes;
+	public           int      OpcaoEscolhida { get; private set; }
 
 	public MenuView(string   titulo,
 	                string   cabecalho,
@@ -28,5 +30,20 @@ public class MenuView : View
 
 		Layout.AppendLine();
 		Layout.Append("Sua opção (somente números): ");
+	}
+
+	public void LerEntrada()
+	{
+		Exibir();
+
+		var entrada       = Console.ReadKey();
+		var entradaValida = int.TryParse(entrada
+			                                 .KeyChar
+			                                 .ToString(), 
+			                                 out var opcaoEscolhida);
+
+		if (!entradaValida) return;
+
+		OpcaoEscolhida = opcaoEscolhida;
 	}
 }
