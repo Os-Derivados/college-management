6c175233bc002171aa41c6d9c6f8680d3f501708
xvitxr <vitor.mon1.com@gmail.com>
2024-12-15 14:30:57 -0300

    refactor: mover CredenciaisUsuario para seu próprio arquivo

---
+++ college-management/Dados/CredenciaisUsuario.cs
@@ -0,0 +1,42 @@
+﻿using college_management.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace college_management.Dados;
+
+public class CredenciaisUsuario
+{
+    public string Senha { get; set; }
+    public string Sal { get; set; }
+
+    public CredenciaisUsuario(string senha, string? sal = null)
+    {
+        Senha = senha;
+        Sal = sal ?? Utilitarios.UtilitarioCriptografia.GerarSal();
+    }
+
+    public static CredenciaisUsuario? TryParse(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return null;
+
+        var split = input.Split('+', 2);
+        if (split.Length <= 1) return null;
+
+        (string senha, string sal) = (split[0], split[1]);
+        return new CredenciaisUsuario(senha, sal);
+    }
+
+    public bool Validar(string senha)
+    {
+        return Utilitarios.UtilitarioCriptografia.CriptografarSha256(senha, Sal) == Senha;
+    }
+
+    public override string ToString()
+    {
+        return $"{Senha}+{Sal}";
+    }
+}
