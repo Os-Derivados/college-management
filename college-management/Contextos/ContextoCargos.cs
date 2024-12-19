af38c1bd9eb9bdda049184bc8e56710d16f0f86d
CsPauloNoob <ps616131@gmail.com>
2024-12-18 22:30:55 -0500

    feature(ContextoCargo/Visualizar): Implementado feature de visualizar cargos

--- college-management/Contextos/ContextoCargos.cs
+++ college-management/Contextos/ContextoCargos.cs
@@ -1,7 +1,9 @@
+using college_management.Constantes;
 using college_management.Contextos.Interfaces;
 using college_management.Dados;
 using college_management.Dados.Modelos;
 using college_management.Dados.Repositorios;
+using college_management.Views;
 
 
 namespace college_management.Contextos;
@@ -14,12 +16,111 @@ public class ContextoCargos : Contexto<Cargo>
 		base(baseDeDados,
 		     usuarioContexto) { }
 
-	public override async Task Cadastrar() { throw new NotImplementedException(); }
+	public override async Task Cadastrar() 
+    {
+
+    }
 
 	public override async Task Editar() { throw new NotImplementedException(); }
 
 	public override async Task Excluir() { throw new NotImplementedException(); }
 
-	public override void Visualizar()  { throw new NotImplementedException(); }
-	public override void VerDetalhes() { throw new NotImplementedException(); }
+	public override void Visualizar()  
+	{
+        var permissaoAdmin = CargoContexto.TemPermissao(PermissoesAcesso.AcessoAdministradores);
+
+
+		if(permissaoAdmin)
+		{
+            OpcoesDeVisualizacao();
+
+            Console.ReadKey();
+		}
+
+    }
+
+
+	public override void VerDetalhes() 
+    {
+
+    }
+
+
+
+
+    #region Metodos privados uteis
+
+	void OpcoesDeVisualizacao()
+	{
+        MenuView menuPesquisa = new("Cargos",
+                                    "Selecione um dos campos:",
+                                    ["Nome do Cargo", "Id", "Exibir Todos"]);
+
+        List<Cargo> cargos = new();
+        RelatorioView<Cargo> relatorioView = null!;
+
+
+        menuPesquisa.ConstruirLayout();
+        menuPesquisa.LerEntrada();
+
+        KeyValuePair<string, string>? campoPesquisa = menuPesquisa.OpcaoEscolhida switch
+        {
+            1 => new KeyValuePair<string, string>("Nome do Cargo",
+                                                  "Insira o Nome do Cargo: "),
+            2 => new KeyValuePair<string, string>("Id",
+                                                  "Insira o Id do Cargo: "),
+
+            3 => new KeyValuePair<string, string>("Exiir Todos", 
+                                                  "Pressione Enter para ver todos os cargos"),
+
+            _ => null
+        };
+
+        InputView inputPesquisa = new("Ver Detalhes: Pesquisar Cargo");
+
+        if (campoPesquisa is null)
+        {
+            inputPesquisa.LerEntrada("Campo",
+                                     "Campo inválido. Tente novamente.");
+
+            return;
+        }
+
+        inputPesquisa.LerEntrada(campoPesquisa?.Key,
+                                 campoPesquisa?.Value);
+
+        
+
+        if (menuPesquisa.OpcaoEscolhida is 1)
+        {
+            var nomeDoCargo = inputPesquisa.ObterEntrada("Nome do Cargo");
+            cargos.Add(BaseDeDados.Cargos.ObterPorNome(nomeDoCargo));
+        }
+
+        else if (menuPesquisa.OpcaoEscolhida is 2)
+        {
+            var id = inputPesquisa.ObterEntrada("Id");
+            cargos.Add(BaseDeDados.Cargos.ObterPorId(id));
+        }
+
+        else if(menuPesquisa.OpcaoEscolhida is 3)
+        {
+            cargos = BaseDeDados.Cargos.ObterTodos();
+        }
+
+        if (!cargos.Any())
+        {
+            inputPesquisa.LerEntrada("Cargo",
+                                     "Cargo não encontrado.");
+            return;
+        }
+
+        relatorioView = new RelatorioView<Cargo>("Cargos", cargos);
+        
+
+        relatorioView.ConstruirLayout();
+        relatorioView.Exibir();
+    }
+
+    #endregion
 }
