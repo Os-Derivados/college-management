00a557a6d95c69231c882ba95f524c1926bb01d3
trsaints <thiago.dev2k3@gmail.com>
2025-01-12 00:51:50 -0300

    feat: criar componente View para armazenar confirmação em fluxos de interações

---
+++ college-management/Views/ConfirmaView.cs
@@ -0,0 +1,21 @@
+using college_management.Views.Interfaces;
+
+
+namespace college_management.Views;
+
+
+public class ConfirmaView : View, IConfirmaView
+{
+	public ConfirmaView(string titulo) : base(titulo)
+	{
+	}
+
+	public char Confirmar(string mensagem)
+	{
+		Console.Clear();
+		Console.WriteLine(mensagem);
+		Console.WriteLine("Deseja confirmar? (S/N)");
+		
+		return Console.ReadKey().KeyChar;
+	}
+}
