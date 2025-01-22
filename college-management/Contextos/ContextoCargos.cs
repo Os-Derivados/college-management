303cae060541c9731dc721bf00fa9d27e61deb67
trsaints <thiago.dev2k3@gmail.com>
2025-01-22 01:03:19 -0300

    update: adaptar contexto para corresponder à nova estrutura de Respostas

--- college-management/Contextos/ContextoCargos.cs
+++ college-management/Contextos/ContextoCargos.cs
@@ -18,424 +18,395 @@ public class ContextoCargos : Contexto<Cargo>
 	{
 	}
 
-	public override async Task Cadastrar() 
-    {
-        var temPermissao =
-        CargoContexto.TemPermissao(PermissoesAcesso.AcessoEscrita) || 
-        CargoContexto.TemPermissao(PermissoesAcesso.AcessoAdministradores);
-
-        Cargo novoCargo = null;
-
-        if (temPermissao)
-        {
-            novoCargo = TelaDeCadastro();
-
-            if (novoCargo is null)
-            {
-                TelaErro("O cargo precisa de um nome e permissões válidas");
-                return;
-            }
-
-            
-
-            if (ConfirmarEscolha("Deseja salvar o cargo: ", novoCargo))
-            {
-                var resultado = await BaseDeDados.Cargos.Adicionar(novoCargo);
-                if (resultado)
-                {
-                    Console.WriteLine("Cargo salvo com sucesso!");
-                    Console.ReadKey();
-                }
-
-                else TelaErro("Cargo não salvo, pois outro com o mesmo nome já existe no banco de dados!");
-            }
-            
-        }
-    }
-
-	public override async Task Editar() 
-    {
-        var temPermissao =
-        CargoContexto.TemPermissao(PermissoesAcesso.AcessoEscrita) ||
-        CargoContexto.TemPermissao(PermissoesAcesso.AcessoAdministradores);
-
-        Cargo? cargo = null;
-        string nomeCargo = "";
-
-
-        InputView inputView = new InputView("--Editor de cargos--");
-
-        if(temPermissao)
-        {
-            nomeCargo = SelecionaCargoParaEdicao(inputView);
-
-            cargo = BaseDeDados.Cargos.ObterPorNome(nomeCargo);
-
-            if (cargo is null)
-            {
-                TelaErro("Cargo não existe");
-                return;
-            }
-
-            cargo = TelaDeEdicao(cargo, inputView);
-
-            if (cargo is null)
-            {
-                TelaErro("O cargo precisa de um nome e permissões válidas");
-                return;
-            }
-
-
-            if (ConfirmarEscolha("", cargo))
-            {
-                await BaseDeDados.Cargos.Atualizar(cargo);
-                inputView.LerEntrada("Sair", "Cargo Editado com sucesso");
-            }
-
-        }
-    }
-
-	public override async Task Excluir() 
-    {
-        var temPermissao =
-        CargoContexto.TemPermissao(PermissoesAcesso.AcessoEscrita) ||
-        CargoContexto.TemPermissao(PermissoesAcesso.AcessoAdministradores);
-
-        InputView inputView = new InputView("Exclusao de Cargo");
-        Cargo cargo = null!;
-
-        if(temPermissao)
-        {
-            cargo = PesquisaCargo();
-
-            if (cargo is null)
-            {
-                TelaErro("Opção Inválida!");
-
-                return;
-            }
-
-
-            else
-            {
-                if (ConfirmarEscolha("Tem certeza que " +
-                    $"deseja excluir o cargo {cargo.Nome}?"))
-                {
-                    await BaseDeDados.Cargos.Remover(cargo.Id);
-                    inputView.LerEntrada("Sair", "Exclusão realizada com sucesso");
-                }
-            }
-            
-        }
-    }
-
-	public override void Visualizar()  
+	public override async Task Cadastrar()
 	{
-        var temPermissao = CargoContexto.TemPermissao(PermissoesAcesso.AcessoAdministradores) ||
-                                           CargoContexto.TemPermissao(PermissoesAcesso.AcessoEscrita);
+		if (TemAcessoRestrito)
+		{
+			var novoCargo = TelaDeCadastro();
 
-        RelatorioView<Cargo> relatorioView;
+			if (novoCargo is null)
+			{
+				TelaErro("O cargo precisa de um nome e permissões válidas");
 
-        if (temPermissao)
-        {
-            relatorioView = new RelatorioView<Cargo>("Visualizar Usuários",
-                                                       BaseDeDados.Cargos.ObterTodos());
-        }
+				return;
+			}
 
-        else
-            relatorioView = new RelatorioView<Cargo>("Visualizar Usuário", new List<Cargo>() 
-            { 
-                BaseDeDados.Cargos.ObterPorId(UsuarioContexto.CargoId) 
-            });
+			if (ConfirmarEscolha("Deseja salvar o cargo: ", novoCargo))
+			{
+				var adicionarCargo
+					= await BaseDeDados.Cargos.Adicionar(novoCargo);
 
+				if (adicionarCargo.Status is StatusResposta.Sucesso)
+				{
+					View.Aviso("Cargo salvo com sucesso!");
 
-        relatorioView.ConstruirLayout();
-        relatorioView.Exibir();
+					return;
+				}
 
+				View.Aviso(
+					"Cargo não salvo, pois outro com o mesmo nome já existe no banco de dados!");
+			}
+		}
+	}
 
-    }
-
-
-	public override void VerDetalhes() 
-    {
-        var permissaoAdmin = CargoContexto.TemPermissao(PermissoesAcesso.AcessoAdministradores);
-
-
-        if (permissaoAdmin)
-        {
-            Cargo cargo = null!;
-            DetalhesView detalhesView = null;
-
-            cargo = PesquisaCargo();
-
-            if (cargo is null)
-            {
-                TelaErro("Opção Inválida!");
-            }
-
-            else
-                ExibirDetalhesCargo(cargo, detalhesView);
-        }
-    }
-
-
-
-
-    #region Metodos privados uteis
-
-    Cargo TelaDeCadastro()
-    {
-        InputView inputUsuario = new("Cadastrar cargo");
-        inputUsuario.ConstruirLayout();
-
-        inputUsuario.LerEntrada("nome", "Insira o nome do novo cargo: ");
-
-        if (ValidarEntrada(inputUsuario, "nome"))
-        {
-
-            List<string> nivelDePermissao = SelecaoDePermissao();
-            string nomeCargo = inputUsuario.EntradasUsuario["nome"];
-
-            if (nomeCargo is not null &&
-                nivelDePermissao.Any())
-            {
-                return new Cargo(nomeCargo, nivelDePermissao);
-            }
-
-        }
-        
-        return null;
-    }
-
-
-
-	Cargo PesquisaCargo()
+	public override async Task Editar()
 	{
-        MenuView menuPesquisa = new("Cargos",
-                                    "Selecione um dos campos:",
-                                    ["Nome do Cargo", "Id"]);
+		Cargo? cargo     = null;
+		string nomeCargo = "";
+
 
-        Cargo cargo = null!;
-        DetalhesView detalhesView = null!;
-        
+		InputView inputView = new InputView("--Editor de cargos--");
+
+		if (TemAcessoRestrito)
+		{
+			nomeCargo = SelecionaCargoParaEdicao(inputView);
 
+			var obterPorNome = BaseDeDados.Cargos.ObterPorNome(nomeCargo);
 
-        menuPesquisa.ConstruirLayout();
-        menuPesquisa.LerEntrada();
+			if (obterPorNome.Status is StatusResposta.ErroNaoEncontrado)
+			{
+				View.Aviso("Cargo não existe");
 
-        KeyValuePair<string, string>? campoPesquisa = menuPesquisa.OpcaoEscolhida switch
-        {
-            1 => new KeyValuePair<string, string>("Nome do Cargo",
-                                                  "Insira o Nome do Cargo: "),
-            2 => new KeyValuePair<string, string>("Id",
-                                                  "Insira o Id do Cargo: "),
+				return;
+			}
 
-            _ => null
-        };
+			cargo = TelaDeEdicao(cargo, inputView);
 
-        InputView inputPesquisa = new("Ver Detalhes: Pesquisar Cargo");
+			if (cargo is null)
+			{
+				TelaErro("O cargo precisa de um nome e permissões válidas");
+				return;
+			}
 
-        if (campoPesquisa is null)
-        {
-            Console.Clear();
-            return null;
-        }
 
+			if (ConfirmarEscolha("", cargo))
+			{
+				await BaseDeDados.Cargos.Atualizar(cargo);
+				inputView.LerEntrada("Sair", "Cargo Editado com sucesso");
+			}
+		}
+	}
 
-        inputPesquisa.LerEntrada(campoPesquisa?.Key,
-                                 campoPesquisa?.Value);
+	public override async Task Excluir()
+	{
+		InputView inputView = new InputView("Exclusao de Cargo");
+		Cargo     cargo     = null!;
 
-        
+		if (TemAcessoRestrito)
+		{
+			cargo = PesquisaCargo();
 
-        if (menuPesquisa.OpcaoEscolhida is 1)
-        {
-            var nomeDoCargo = inputPesquisa.ObterEntrada("Nome do Cargo");
-            cargo = BaseDeDados.Cargos.ObterPorNome(nomeDoCargo);
-        }
+			if (cargo is null)
+			{
+				TelaErro("Opção Inválida!");
 
-        else if (menuPesquisa.OpcaoEscolhida is 2)
-        {
-            var id = inputPesquisa.ObterEntrada("Id");
-            cargo = BaseDeDados.Cargos.ObterPorId(id);
-        }
+				return;
+			}
 
-        Console.Clear();
 
-        return cargo;
-    }
+			else
+			{
+				if (ConfirmarEscolha("Tem certeza que " +
+				                     $"deseja excluir o cargo {cargo.Nome}?"))
+				{
+					await BaseDeDados.Cargos.Remover(cargo.Id);
+					inputView.LerEntrada(
+						"Sair", "Exclusão realizada com sucesso");
+				}
+			}
+		}
+	}
 
+	public override void Visualizar()
+	{
+		RelatorioView<Cargo> relatorioView;
 
-    List<string> SelecaoDePermissao()
-    {
-        List<string> permissoes = new List<string>();
+		if (TemAcessoRestrito)
+		{
+			relatorioView = new RelatorioView<Cargo>("Visualizar Usuários",
+				BaseDeDados.Cargos.ObterTodos().Modelo!);
+		}
+		else
+		{
+			relatorioView = new RelatorioView<Cargo>("Visualizar Usuário",
+			[
+				BaseDeDados.Cargos.ObterPorId(UsuarioContexto.CargoId).Modelo!
+			]);
+		}
 
-        var propriedades = typeof(PermissoesAcesso).GetFields();
-        string[] nomePropriedades = new string[propriedades.Length];
-        int index = 0;
+		relatorioView.ConstruirLayout();
+		relatorioView.Exibir();
+	}
 
-        for (int i = 0; i < propriedades.Length; i++)
-        {
-            nomePropriedades[i] = propriedades[i].Name;
-            index++;
-        }
-        index = 0;
-        
 
-        while(index < nomePropriedades.Length)
-        {
-            MenuView menuView = new 
-                MenuView("Permissões", 
-                "\t\tPermissões\n\n\n", 
-                nomePropriedades);
+	public override void VerDetalhes()
+	{
+		if (!TemAcessoRestrito) return;
 
-            menuView.ConstruirLayout();
-            menuView.Exibir();
-            menuView.LerEntrada();
+		DetalhesView detalhesView = null;
 
-            int opcao = menuView.OpcaoEscolhida-1;
+		var cargo = PesquisaCargo();
 
-            if (opcao >= nomePropriedades.Length || opcao < 0) break;
+		if (cargo is null)
+		{
+			TelaErro("Opção Inválida!");
 
-            if (!permissoes.Contains
-                (nomePropriedades
-                [opcao]))
+			return;
+		}
 
-                permissoes.Add(nomePropriedades[opcao]);
+		ExibirDetalhesCargo(cargo, detalhesView);
+	}
 
 
+	#region Metodos privados uteis
 
-            if (ConfirmarEscolha($"Adicionou {permissoes.Last()}" +
-                $", deseja adicionar mais permissões?\n"))
+	Cargo? TelaDeCadastro()
+	{
+		InputView inputUsuario = new("Cadastrar cargo");
+		inputUsuario.ConstruirLayout();
 
-                index = opcao;
+		inputUsuario.LerEntrada("nome", "Insira o nome do novo cargo: ");
 
-            else break;
+		if (ValidarEntrada(inputUsuario, "nome"))
+		{
+			List<string> nivelDePermissao = SelecaoDePermissao();
+			string       nomeCargo = inputUsuario.EntradasUsuario["nome"];
 
+			if (nomeCargo is not null &&
+			    nivelDePermissao.Any())
+			{
+				return new Cargo(nomeCargo, nivelDePermissao);
+			}
+		}
 
-        }
+		return null;
+	}
 
-        Console.Clear();
-        return permissoes;
-        
-    }
 
+	Cargo? PesquisaCargo()
+	{
+		MenuView menuPesquisa = new("Cargos",
+		                            "Selecione um dos campos:",
+		                            ["Nome do Cargo", "Id"]);
 
-    string TelaExclusao(InputView inputView)
-    {
+		Cargo        cargo        = null!;
+		DetalhesView detalhesView = null!;
 
-        inputView.LerEntrada("name", "Insira o nome do cargo a ser excluido do banco de dados: ");
 
-        var nomeCargo = inputView.ObterEntrada("name");
+		menuPesquisa.ConstruirLayout();
+		menuPesquisa.LerEntrada();
 
-        return nomeCargo;
-    }
+		KeyValuePair<string, string>? campoPesquisa
+			= menuPesquisa.OpcaoEscolhida switch
+			{
+				1 => new KeyValuePair<string, string>("Nome",
+					"Insira o Nome do Cargo: "),
+				2 => new KeyValuePair<string, string>("Id",
+					"Insira o Id do Cargo: "),
 
-    string SelecionaCargoParaEdicao(InputView inputView)
-    {
+				_ => null
+			};
 
-        inputView.LerEntrada("name", "Insira o nome do cargo a ser editado: ");
+		InputView inputPesquisa = new("Ver Detalhes: Pesquisar Cargo");
 
-        var cargoId = inputView.ObterEntrada("name");
+		if (campoPesquisa is null)
+		{
+			Console.Clear();
+			return null;
+		}
 
-        return cargoId;
-    }
+		inputPesquisa.LerEntrada(campoPesquisa?.Key,
+		                         campoPesquisa?.Value);
 
+		switch (menuPesquisa.OpcaoEscolhida)
+		{
+			case 1:
+			{
+				var nomeDoCargo = inputPesquisa.ObterEntrada("Nome");
+				cargo = BaseDeDados.Cargos.ObterPorNome(nomeDoCargo).Modelo!;
 
-    Cargo? TelaDeEdicao(Cargo cargoAtual, InputView inputView)
-    {
-        inputView.LerEntrada("nome",
-            $"Nome Atual: {cargoAtual.Nome}\n\nEscolha um novo nome: ");
-        if (ValidarEntrada(inputView, "nome"))
-        {
-            List<string> permissoes = SelecaoDePermissao();
-            if (!permissoes.Any()) return null;
+				break;
+			}
+			case 2:
+			{
+				var id = inputPesquisa.ObterEntrada("Id");
+				cargo = BaseDeDados.Cargos.ObterPorId(id).Modelo!;
 
-            return new Cargo(inputView.ObterEntrada("nome"), permissoes)
-            {
-                Id = cargoAtual.Id 
-            };
-        }
-        return null;
-    }
+				break;
+			}
+		}
 
+		Console.Clear();
 
+		return cargo;
+	}
 
-    bool ValidarEntrada(InputView inputView, string chave)
-    {
 
-        string item = inputView.ObterEntrada(chave);
+	List<string> SelecaoDePermissao()
+	{
+		List<string> permissoes = new List<string>();
 
+		var      propriedades     = typeof(PermissoesAcesso).GetFields();
+		string[] nomePropriedades = new string[propriedades.Length];
+		int      index            = 0;
 
-        if (string.IsNullOrEmpty(item)) 
-        {
-            return false; 
-        }
+		for (int i = 0; i < propriedades.Length; i++)
+		{
+			nomePropriedades[i] = propriedades[i].Name;
+			index++;
+		}
 
-        else return true;
-    }
+		index = 0;
 
-    void TelaErro(string menssagem)
-    {
-        Console.Clear();
 
-        Console.WriteLine(menssagem);
-        Console.WriteLine("\t\t\n\n <--- Pressione alguma tecla para Sair --->");
-        Console.ReadKey();
-    }
+		while (index < nomePropriedades.Length)
+		{
+			MenuView menuView = new
+				MenuView("Permissões",
+				         "\t\tPermissões\n\n\n",
+				         nomePropriedades);
 
-    void ExibirDetalhesCargo(Cargo cargo, DetalhesView detalhesView)
-    {
-        var dicionario = new Dictionary<string, string>();
-        string permissoes = ListaParaString(cargo.Permissoes);
-        string usuarioId = ListaParaString(cargo.UsuariosIds);
+			menuView.ConstruirLayout();
+			menuView.Exibir();
+			menuView.LerEntrada();
 
-        dicionario.Add("Id", cargo.Id);
-        dicionario.Add("Nome", cargo.Nome);
-        dicionario.Add("Permissões", permissoes);
-        dicionario.Add("Ids de Usuários", usuarioId);
+			int opcao = menuView.OpcaoEscolhida - 1;
 
-        detalhesView = new DetalhesView("Cargo", dicionario);
+			if (opcao >= nomePropriedades.Length || opcao < 0) break;
 
-        detalhesView.ConstruirLayout();
-        detalhesView.Exibir();
-    }
+			if (!permissoes.Contains
+			    (nomePropriedades
+				     [opcao]))
 
-    string ListaParaString(List<string> strings)
-    {
-        string output = "\n[\n";
-        foreach (string str in strings) 
-        {
-            output += $"\t{str}\n";
-        }
+				permissoes.Add(nomePropriedades[opcao]);
 
-        output += "]";
 
-        return output;
-    }
+			if (ConfirmarEscolha($"Adicionou {permissoes.Last()}" +
+			                     $", deseja adicionar mais permissões?\n"))
 
+				index = opcao;
 
-    bool ConfirmarEscolha(string menssagem, Cargo cargo = null)
-    {
-        Console.Clear();
-        Console.WriteLine(menssagem);
+			else break;
+		}
 
-        if (cargo is not null)
-        {
-            DetalhesView detalhesView = null;
-            ExibirDetalhesCargo(cargo, detalhesView);
-        }
+		Console.Clear();
+		return permissoes;
+	}
 
-        Console.WriteLine("\n\nPara confirmar:\n\n[S]im ou [N]ão: ");
 
-        var opcao = Console.ReadKey();
+	string TelaExclusao(InputView inputView)
+	{
+		inputView.LerEntrada(
+			"name",
+			"Insira o nome do cargo a ser excluido do banco de dados: ");
 
-        Console.Clear();
+		var nomeCargo = inputView.ObterEntrada("name");
 
-        if (opcao.KeyChar == 's' || opcao.KeyChar == 'S')
-            return true;
+		return nomeCargo;
+	}
 
-        else return false;
-    }
+	string SelecionaCargoParaEdicao(InputView inputView)
+	{
+		inputView.LerEntrada("name", "Insira o nome do cargo a ser editado: ");
 
-    #endregion
+		var cargoId = inputView.ObterEntrada("name");
+
+		return cargoId;
+	}
+
+
+	Cargo? TelaDeEdicao(Cargo cargoAtual, InputView inputView)
+	{
+		inputView.LerEntrada("nome",
+		                     $"Nome Atual: {cargoAtual.Nome}\n\nEscolha um novo nome: ");
+
+		if (!ValidarEntrada(inputView, "nome")) return null;
+
+		var permissoes = SelecaoDePermissao();
+		if (!permissoes.Any()) return null;
+
+		return new Cargo(inputView.ObterEntrada("nome"), permissoes)
+		{
+			Id = cargoAtual.Id
+		};
+	}
+
+
+	bool ValidarEntrada(InputView inputView, string chave)
+	{
+		string item = inputView.ObterEntrada(chave);
+
+
+		if (string.IsNullOrEmpty(item))
+		{
+			return false;
+		}
+
+		else return true;
+	}
+
+	void TelaErro(string menssagem)
+	{
+		Console.Clear();
+
+		Console.WriteLine(menssagem);
+		Console.WriteLine(
+			"\t\t\n\n <--- Pressione alguma tecla para Sair --->");
+		Console.ReadKey();
+	}
+
+	void ExibirDetalhesCargo(Cargo cargo, DetalhesView detalhesView)
+	{
+		var    dicionario = new Dictionary<string, string>();
+		string permissoes = ListaParaString(cargo.Permissoes);
+
+		dicionario.Add("Id", cargo.Id);
+		dicionario.Add("Nome", cargo.Nome);
+		dicionario.Add("Permissões", permissoes);
+
+		detalhesView = new DetalhesView("Cargo", dicionario);
+
+		detalhesView.ConstruirLayout();
+		detalhesView.Exibir();
+	}
+
+	string ListaParaString(List<string> strings)
+	{
+		string output = "\n[\n";
+
+		foreach (string str in strings)
+		{
+			output += $"\t{str}\n";
+		}
+
+		output += "]";
+
+		return output;
+	}
+
+
+	bool ConfirmarEscolha(string menssagem, Cargo? cargo = null)
+	{
+		Console.Clear();
+		Console.WriteLine(menssagem);
+
+		if (cargo is not null)
+		{
+			DetalhesView detalhesView = null;
+			ExibirDetalhesCargo(cargo, detalhesView);
+		}
+
+		Console.WriteLine("\n\nPara confirmar:\n\n[S]im ou [N]ão: ");
+
+		var opcao = Console.ReadKey();
+
+		Console.Clear();
+
+		if (opcao.KeyChar == 's' || opcao.KeyChar == 'S')
+			return true;
+
+		return false;
+	}
+
+	#endregion
 }
