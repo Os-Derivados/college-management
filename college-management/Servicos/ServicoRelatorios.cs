7e28e13050e4e49599498d5d5c81ab5f9071210e
mathpss <matheus.p.souza.silva@gmail.com>
2025-01-24 21:37:00 -0300

    finalizado ExportarRelatorio

--- college-management/Servicos/ServicoRelatorios.cs
+++ college-management/Servicos/ServicoRelatorios.cs
@@ -94,7 +94,7 @@ where T : Modelo
 
 	public async Task ExportarRelatorio(string relatorio)
 	{
-		// TODO: Implementar um algoritmo para exportar relatórios no formato CSV
+		
 
 
             File.WriteAllText(_arquivoRelatorios, relatorio);
