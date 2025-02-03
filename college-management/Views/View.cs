caa9fbe2e6c503d7213361fa85e3fc427d47bdfb
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-02-02 23:45:21 -0300

    fix errors

--- college-management/Views/View.cs
+++ college-management/Views/View.cs
@@ -1,6 +1,6 @@
 using System.Text;
 using college_management.Views.Interfaces;
-using college_management.Utilitarios; // Import Logger
+using college_management.Utilitarios;
 
 namespace college_management.Views;
 
@@ -8,7 +8,7 @@ public abstract class View : IView
 {
 	public readonly StringBuilder Layout = new();
 	public readonly string Titulo;
-	private static readonly Logger logger = new Logger(); // Initialize Logger
+	private static readonly Logger logger = new Logger();
 
 	protected View(string titulo) { Titulo = titulo; }
 
@@ -22,10 +22,10 @@ public abstract class View : IView
 
 	public static void Aviso(string mensagem)
 	{
-		// Log the warning message
+
 		logger.Log($"Aviso: {mensagem}", Logger.LogLevel.Warn);
 
-		// Display the message to the user
+
 		InputView inputAviso = new("Aviso");
 		inputAviso.ConstruirLayout();
 		inputAviso.LerEntrada("Aviso", $"""
