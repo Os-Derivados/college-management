ef109146f103f1fe17a8571c481140c84fabab1e
xvitxr <vitor.mon1.com@gmail.com>
2025-03-23 18:48:57 -0300

    update: interface especial para views pagináveis

---
+++ college-management/Views/Interfaces/IPaginavel.cs
@@ -0,0 +1,16 @@
+namespace college_management.Views.Interfaces;
+
+/// <summary>
+/// Usado em Views que possuem a possibildade de serem paginadas por <see cref="PaginaView"/>.
+/// </summary>
+public interface IPaginavel
+{
+    /// <summary>
+    /// Constrói uma série de páginas que serão utilizadas por <see cref="PaginaView"/>.
+    /// </summary>
+    /// <param name="linhasMaximas">
+    ///     Parâmetro arbitrário utilizado para determinar o tamanho máximo de cada página,
+    ///     especificado em linhas no console. Geralmente corresponde à metade da altura do console. 
+    /// </param>
+    public string[] ConstruirPaginas(int linhasMaximas);
+}
--- college-management/Views/RelatorioView.cs
+++ college-management/Views/RelatorioView.cs
@@ -1,11 +1,13 @@
+using System.Text;
 using college_management.Dados.Modelos;
 using college_management.Utilitarios;
+using college_management.Views.Interfaces;
 
 
 namespace college_management.Views;
 
 
-public class RelatorioView<T> : View where T : Modelo
+public class RelatorioView<T> : View, IPaginavel where T : Modelo
 {
 	private readonly List<T> _modelos;
 
@@ -35,11 +37,53 @@ public class RelatorioView<T> : View where T : Modelo
 		foreach (var modelo in _modelos)
 			Layout.AppendLine(modelo.ToString());
 	}
-
+	
 	public override void Exibir()
 	{
 		base.Exibir();
 
 		Console.ReadLine();
+	}
+
+	public string[] ConstruirPaginas(int linhasMaximas)
+	{
+		int indice = 0;
+		int linhas = 0;
+		List<StringBuilder> conteudo = [new()];
+		StringBuilder layout = conteudo[indice];
+		
+		var tipo         = typeof(T);
+		var propriedades = tipo.GetProperties();
+		var nomesPropriedades =
+			UtilitarioTipos.ObterNomesPropriedades(propriedades);
+
+		layout.AppendLine(nomesPropriedades);
+		foreach (var p in propriedades)
+			layout.Append($"| {new string('-', 16)} ");
+		layout.AppendLine("|");
+		linhas += 2;
+		
+		foreach (var modelo in _modelos)
+		{
+			if (linhas > linhasMaximas)
+			{
+				linhas = 0;
+				indice++;
+				conteudo.Add(new());
+				layout = conteudo[indice];
+				layout.AppendLine(nomesPropriedades);
+				foreach (var p in propriedades)
+					layout.Append($"| {new string('-', 16)} ");
+				layout.AppendLine("|");
+				linhas += 2;
+				
+				continue;
+			}
+
+			layout.AppendLine(modelo.ToString());
+			linhas++;
+		}
+
+		return conteudo.Select(i => i.ToString()).ToArray();
 	}
 }
