bc0ba4cdef79842772a3289663b84d23f86e9be2
haran venetillo <haran08@hotmail.com>
2025-03-24 10:37:58 -0300

    Remoção do método redundante no banco de dados.

--- college-management/Dados/BaseDeDados.cs
+++ college-management/Dados/BaseDeDados.cs
@@ -12,30 +12,4 @@ public sealed class BaseDeDados
 	public readonly RepositorioMaterias   Materias   = new();
 	public readonly RepositorioMatriculas Matriculas = new();
 	public readonly RepositorioUsuarios   Usuarios   = new();
-
-
-
-	public object EscolherBaseDeDados<T>() where T : Modelo
-	{
-		switch(typeof(T))
-		{
-			case Type cargo     when cargo	   == typeof(Cargo):
-				return Cargos.ObterBaseDeDados();
-
-			case Type curso     when curso     == typeof(Curso):
-				return Cursos.ObterBaseDeDados();
-
-            case Type materia   when materia   == typeof(Materia):
-                return Materias.ObterBaseDeDados();
-
-            case Type matricula when matricula == typeof(Matricula):
-                return Matriculas.ObterBaseDeDados();
-
-            case Type usuario   when usuario   == typeof(Usuario):
-                return Usuarios.ObterBaseDeDados();
-
-			default:
-				throw new NotImplementedException($"Error: Tipo não implementado. (BaseDeDados.EscolherBaseDeDados)");
-        }
-	}
 }
