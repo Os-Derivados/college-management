1b7cb2f0e52753fded5d0090682828d6a170d43b
trsaints <thiago.dev2k3@gmail.com>
2025-04-06 20:36:15 -0300

    update: remover abstração desnecessária para injeção de dependência

--- college-management/Program.cs
+++ college-management/Program.cs
@@ -1,17 +1,25 @@
 ﻿using college_management.Dados;
 using college_management.Dados.Contexto;
+using college_management.Dados.Repositorios;
 using college_management.Middlewares;
 using college_management.Utilitarios;
 using college_management.Views;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 UtilitarioArquivos.Inicializar();
 
-ServiceCollection servicos = new();
-ConfigurarServicos(servicos);
+var options = new DbContextOptionsBuilder<BancoDeDados>()
+              .UseSqlite(
+	              $"Data Source={Path.Combine(UtilitarioArquivos.DiretorioDados, "college_management.db")}")
+              .Options;
 
-BaseDeDados baseDeDados = new();
+await using var bancoDeDados = new BancoDeDados(options);
+
+var baseDeDados = new BaseDeDados(new RepositorioAvaliacoes(bancoDeDados),
+                                  new RepositorioCursos(bancoDeDados),
+                                  new RepositorioMaterias(bancoDeDados),
+                                  new RepositorioMatriculas(bancoDeDados),
+                                  new RepositorioUsuarios(bancoDeDados));
 
 if (!bool.TryParse(args[1], out var seed))
 	View.Aviso(
@@ -25,7 +33,6 @@ else if (!UtilitarioSeed.ValidarDadosIniciais(baseDeDados))
 {
 	View.Aviso(
 		"Base de Dados não inicializada com valores padrão. Execute o programa novamente com o argumento seed definido como true");
-
 	return;
 }
 
@@ -33,22 +40,11 @@ if (!bool.TryParse(args[0], out var modoDesenvolvimento))
 	View.Aviso(
 		"Aviso : Argumento modoDesenvolvimento não informado ou incorreto, utilizando o valor padrão : false");
 
-var usuarioLogado =
-	MiddlewareAutenticacao.Autenticar(modoDesenvolvimento,
-	                                  baseDeDados.Usuarios);
+var usuarioLogado
+	= MiddlewareAutenticacao.Autenticar(modoDesenvolvimento,
+	                                    baseDeDados.Usuarios);
 
 MiddlewareContexto.Inicializar(baseDeDados, usuarioLogado);
 
 Console.Clear();
 Console.WriteLine("Saindo...");
-
-return;
-
-static void ConfigurarServicos(IServiceCollection servicos)
-{
-	servicos.AddDbContext<BancoDeDados>(options =>
-	{
-		options.UseSqlite(
-			$"Data Source={Path.Combine(UtilitarioArquivos.DiretorioDados, "college_management.db")}");
-	});
-}
