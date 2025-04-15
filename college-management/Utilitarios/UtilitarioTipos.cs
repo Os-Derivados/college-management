197f63d84d18b2a2cabbb724a2d39445dae62aac
xvitxr <vitor.mon1.com@gmail.com>
2025-04-14 23:15:55 -0300

    refactor: substituir checagem de if com switch

--- college-management/Utilitarios/UtilitarioTipos.cs
+++ college-management/Utilitarios/UtilitarioTipos.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Reflection;
-using System.Text;
 using college_management.Utilitarios.Atributos;
 
 
@@ -35,10 +34,16 @@ public static class UtilitarioTipos
 	private static string ObterValor<T>(T modelo, PropertyInfo propriedade)
 	{
 		var atributo = propriedade.GetCustomAttribute<PropriedadeModeloAttribute>();
-		if (atributo is null || atributo.Tipo is TipoPropriedade.Valor)
-			return propriedade.GetValue(modelo)?.ToString() 
-			       ?? string.Empty;
 
-		return $"{(propriedade.GetValue(modelo) as ICollection)?.Count ?? 0} {atributo.Identificador}";
+		return atributo switch
+		{
+			null or { Tipo: TipoPropriedade.Valor } =>
+				propriedade.GetValue(modelo)?.ToString() ?? string.Empty,
+			{ Tipo: TipoPropriedade.Colecao } =>
+				string.Join(", ", propriedade.GetValue(modelo) as IEnumerable),
+			{ Tipo: TipoPropriedade.Quantidade } =>
+				$"{(propriedade.GetValue(modelo) as ICollection)?.Count ?? 0} {atributo.Identificador}",
+			_ => string.Empty // Deve ser impossível
+		};
 	}
 }
