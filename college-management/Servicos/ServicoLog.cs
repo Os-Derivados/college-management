f25731e6be2ac194e9cae992d5ba2d119b077f1d
xvitxr <vitor.mon1.com@gmail.com>
2025-04-15 14:48:39 -0300

    refactor: transformar namespace para escopo total do arquivo

--- college-management/Servicos/ServicoLog.cs
+++ college-management/Servicos/ServicoLog.cs
@@ -1,34 +1,33 @@
-namespace college_management.Servicos
+namespace college_management.Servicos;
+
+public class ServicoLog
 {
-    public class ServicoLog
+    private readonly string _logDiretorio;
+    private readonly string _logArquivo;
+
+    public ServicoLog(string logDiretorio = "logs")
     {
-        private readonly string _logDiretorio;
-        private readonly string _logArquivo;
+        _logDiretorio = logDiretorio;
 
-        public ServicoLog(string logDiretorio = "logs")
+        if (!Directory.Exists(_logDiretorio))
         {
-            _logDiretorio = logDiretorio;
-
-            if (!Directory.Exists(_logDiretorio))
-            {
-                Directory.CreateDirectory(_logDiretorio);
-            }
+            Directory.CreateDirectory(_logDiretorio);
+        }
             
-            _logArquivo = Path.Combine(_logDiretorio, "logfile.txt");
-        }
+        _logArquivo = Path.Combine(_logDiretorio, "logfile.txt");
+    }
 
-        public void Log(string mensagem, Severidade severidade = Severidade.Info)
-        {
-            string logMensagem = $"{DateTime.Now:dd-MM-yyyy HH:mm:ss} [{severidade}] - {mensagem}";
+    public void Log(string mensagem, Severidade severidade = Severidade.Info)
+    {
+        string logMensagem = $"{DateTime.Now:dd-MM-yyyy HH:mm:ss} [{severidade}] - {mensagem}";
 
-            File.AppendAllText(_logArquivo, logMensagem + Environment.NewLine);
-        }
+        File.AppendAllText(_logArquivo, logMensagem + Environment.NewLine);
+    }
 
-        public enum Severidade
-        {
-            Info,
-            Aviso,
-            Erro,
-        }
+    public enum Severidade
+    {
+        Info,
+        Aviso,
+        Erro,
     }
 }
