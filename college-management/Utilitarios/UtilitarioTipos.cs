5f9485afd21c68638112426feee30c59b71c8a1a
trsaints <thiago.dev2k3@gmail.com>
2025-05-25 18:52:39 -0300

    refactor: renomear parâmetro de método estático para manter coerência de nomenclaturas

--- college-management/Utilitarios/UtilitarioTipos.cs
+++ college-management/Utilitarios/UtilitarioTipos.cs
@@ -8,12 +8,12 @@ namespace college_management.Utilitarios;
 public static class UtilitarioTipos
 {
 	public static Dictionary<string, string> ObterPropriedades<T>(T modelo,
-		string[] nomesPropriedades)
+		string[] campos)
 	{
 		Dictionary<string, string> resultado  = new();
 		var                        tipoModelo = typeof(T);
 
-		foreach (var nome in nomesPropriedades)
+		foreach (var nome in campos)
 		{
 			var propriedade = tipoModelo.GetProperty(nome);
 			var valor = propriedade?.GetValue(modelo)?.ToString() ??
