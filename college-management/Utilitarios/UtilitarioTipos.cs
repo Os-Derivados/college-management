dfd26adcef30849d340869a027b4dc2205894809
trsaints <thiago.dev2k3@gmail.com>
2025-05-25 18:51:55 -0300

    remove: remover método substituído

--- college-management/Utilitarios/UtilitarioTipos.cs
+++ college-management/Utilitarios/UtilitarioTipos.cs
@@ -7,18 +7,6 @@ namespace college_management.Utilitarios;
 
 public static class UtilitarioTipos
 {
-	public static string ObterNomesPropriedades(PropertyInfo[] infos)
-	{
-		StringBuilder propriedades = new();
-
-		foreach (var p in infos)
-			propriedades.Append($"| {p.Name.PadRight(16)} ");
-
-		propriedades.Append('|');
-
-		return propriedades.ToString();
-	}
-
 	public static Dictionary<string, string> ObterPropriedades<T>(T modelo,
 		string[] nomesPropriedades)
 	{
